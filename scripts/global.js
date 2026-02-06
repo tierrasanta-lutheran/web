d55f6a58ca89c50f10acb04a613e101ceebfd3f6
tcolson-tlc <tcolson@tierrasantalutheran.org>
2026-02-06 02:06:07 -0800

    Update global.js

--- scripts/global.js
+++ scripts/global.js
@@ -4,7 +4,21 @@ function window.sayHello() {
   alert("hello");
 }
 
+function sayHello() {
+  alert("hello");
+}
+
 function window.AddGlobalBlurb(event, text) {
+        const container = document.createElement("div");
+        container.className = "recurrence-date";
+  const textToInsert = text ?? "Hey!  This is injected text";
+        container.innerHTML = `<div class="recurrence-date-display">${textToInsert}</div>`;
+
+        // Append it wherever you want — here we add it at the bottom
+        event.appendChild(container);
+}
+
+function AddGlobalBlurb(event, text) {
         const container = document.createElement("div");
         container.className = "recurrence-date";
   const textToInsert = text ?? "Hey!  This is injected text";
