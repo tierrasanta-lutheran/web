34004f1ac663ee366667c6dbc46a6732f30c6de0
tcolson-tlc <tcolson@tierrasantalutheran.org>
2026-02-06 00:40:29 -0800

    Create global.js
    Initial commit.

---
+++ scripts/global.js
@@ -0,0 +1,13 @@
+function sayHello() {
+  alert("hello");
+}
+
+function AddGlobalBlurb(event, text) {
+        const container = document.createElement("div");
+        container.className = "recurrence-date";
+  const textToInsert = text ?? "Hey!  This is injected text";
+        container.innerHTML = `<div class="recurrence-date-display">${textToInsert}</div>`;
+
+        // Append it wherever you want — here we add it at the bottom
+        event.appendChild(container);
+}
